41303068b79e777717c53d04f63e1fdbe4f32575
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2021-10-28 20:32:22 -0500

    VALIDADO - ARCHIVO SI EXISTE

--- 1.py
+++ 1.py
@@ -1,6 +1,6 @@
-import numpy as np
+#import numpy as np
 
-Captura_Numero = []
+#Captura_Numero = []
 
 def Crear():
     archivo = open("inicial.txt","w")
@@ -19,31 +19,23 @@ def Escribir():
 
 
 def Leer():
-        try:
-            archivo =open("inicial.txt","r")
-            print("error al archivo",archivo)
+    try:    
+        archivo = open("inicial12.txt")
+        linea = archivo.readline()
+        while(linea):
+            print(linea)
             linea = archivo.readline()
-            while(linea):
-                print('ola mundo',linea)
-                #Captura_Numero.append(linea)
-                linea = archivo.readline()
-            archivo.close()
-        except FileNotFoundError:
-            print ("NO EXISTE EL ARCHIVO")
-
-
-#print('MATRIZ EN MAYUSCULA ',matriz[0][3])
-# #a = [[1, 2, 3, 4], [5, 6,10,5], [7, 8, 9,4]]
-
-#matrix = [] ; 
-
-#matrix.append([1,2,3,4])
-
-#print(matrix[0][3])
+        archivo.close()
+    except FileNotFoundError:
+        print ("NO EXISTE EL ARCHIVO")
+  
 
 
 
 
 
-#Leer()
+
+Leer()
 input()
+
+
