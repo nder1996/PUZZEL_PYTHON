e7158f07f64f95f9fdb4092657b1b322f9cbabbc
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2021-10-28 20:20:54 -0500

    Borrado

--- 1.py
+++ 1.py
@@ -1,21 +1,49 @@
+import numpy as np
 
+Captura_Numero = []
 
 def Crear():
-    archivo = open("Busqueda.txt","w")
+    archivo = open("inicial.txt","w")
     archivo.close()
 
 def Escribir():
-    archivo = open("Busqueda.txt","a")
-    print("escribe un texto")
-    cadena = input()
-    archivo.write(cadena+'\n')
-    archivo.close()
+    try:
+        archivo = open("inicial.txt")
+        print("escribe un texto")
+        cadena = input()
+        archivo.write(cadena+'\n')
+        archivo.close()
+    except FileNotFoundError:
+        print ("NO EXISTE EL ARCHIVO")
+
+
 
 def Leer():
-    archivo = open("Busqueda.txt","r")
-    linea = archivo.readline()
-    while(linea):
-        print(linea)
-        linea = archivo.readline()
-    archivo.close()
+        try:
+            archivo =open("inicial.txt","r")
+            print("error al archivo",archivo)
+            linea = archivo.readline()
+            while(linea):
+                print('ola mundo',linea)
+                #Captura_Numero.append(linea)
+                linea = archivo.readline()
+            archivo.close()
+        except FileNotFoundError:
+            print ("NO EXISTE EL ARCHIVO")
 
+
+#print('MATRIZ EN MAYUSCULA ',matriz[0][3])
+# #a = [[1, 2, 3, 4], [5, 6,10,5], [7, 8, 9,4]]
+
+#matrix = [] ; 
+
+#matrix.append([1,2,3,4])
+
+#print(matrix[0][3])
+
+
+
+
+
+#Leer()
+input()
