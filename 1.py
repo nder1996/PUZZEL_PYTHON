8943fa675cedd6ea5949ffc72090598aa1991bbe
Ander_AAM <72629491+nder1996@users.noreply.github.com>
2021-10-29 02:00:39 -0500

    numero.,

--- 1.py
+++ 1.py
@@ -1,6 +1,18 @@
 #import numpy as np
 
-#Captura_Numero = []
+Captura_Numero = []
+
+
+
+def Validar_Numero(numero):
+    #print('estos son los numeros ',numero)
+    for i in numero:
+        print(numero.isnumeric())
+       # if i.isnumeric()==True:
+     #       print('el numero del vector ')
+    #    else:
+    #        print('EL VECTOR TIENE UNA LETRA')
+
 
 def Crear():
     archivo = open("inicial.txt","w")
@@ -20,15 +32,21 @@ def Escribir():
 
 def Leer():
     try:    
-        archivo = open("inicial12.txt")
+        archivo = open("inicial.txt")
         linea = archivo.readline()
         while(linea):
-            print(linea)
+            Validar_Numero(linea)
+            #Captura_Numero.append(linea)
+            #print('este son los numeros',linea)
             linea = archivo.readline()
         archivo.close()
     except FileNotFoundError:
         print ("NO EXISTE EL ARCHIVO")
-  
+
+
+
+
+
 
 
 
@@ -36,6 +54,7 @@ def Leer():
 
 
 Leer()
-input()
+#input()
+
 
 
